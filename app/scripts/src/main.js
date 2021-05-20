b7b49fbab1d4a0ec49fe7e377f1c5ba5502e53b7
LisaHong <lxsauce@gmail.com>
2021-05-19 21:15:17 -0700

    Add files via upload
    Final commit

---
+++ app/scripts/src/main.js
@@ -0,0 +1,11 @@
+/*import ChatApp from './app';
+new ChatApp();
+
+function enter() { 
+    document.getElementById("passwordPopup").style.display = "none";
+    var password = document.getElementById("pass").value;
+    if(pass == 'swordfish')
+    {
+        return true;
+    }
+};*/
